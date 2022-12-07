0262d334137a2e37aa6c37c594c8819fb416481a
Amanda Gibson <gibsonag@umich.edu>
2022-12-07 16:30:36 -0500

    Update cortPlateWidget - more replicate mouseID options, fix STD curve

--- Stress/cortPlateWidget/scriptEditor.js
+++ Stress/cortPlateWidget/scriptEditor.js
@@ -1,7 +1,106 @@
 my_widget_script =
 {
     init: function (mode, json_data) {
-        //this method is called when the form is being constructed
+        // jQuery for bootstrap
+        this.include(
+            "https://code.jquery.com/jquery-3.5.1.min.js",
+            "sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=",
+            "anonymous",
+            ()=>{
+                $(document).ready(
+                    ()=>{
+                        // console.log("After load", $.fn.jquery);
+                        
+                        // Load bootstrap
+                        this.include(
+                            "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js",
+                            "sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl",
+                            "anonymous",
+                            ()=>{
+                                $(document).ready(
+                                    ()=>{
+                                        // Load Luxon
+                                        this.include(
+                                            "https://cdn.jsdelivr.net/npm/luxon@1.26.0/build/global/luxon.min.js",
+                                            "sha256-4sbTzmCCW9LGrIh5OsN8V5Pfdad1F1MwhLAOyXKnsE0=",
+                                            "anonymous",
+                                            ()=>{
+                                                $(document).ready(
+                                                    ()=>{
+                                                        // Load bootbox - need the bootstrap JS to be here first, with appropriate jQuery
+                                                        this.include(
+                                                            "https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js",
+                                                            "sha512-RdSPYh1WA6BF0RhpisYJVYkOyTzK4HwofJ3Q7ivt/jkpW6Vc8AurL1R+4AUcvn9IwEKAPm/fk7qFZW3OuiUDeg==",
+                                                            "anonymous",
+                                                            // referrerpolicy="no-referrer"
+                                                            ()=>{
+                                                                $(document).ready(
+                                                                    ()=>{
+                                                                        // selectize
+                                                                        this.include(
+                                                                            "https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.15.0/js/selectize.js", 
+                                                                            "sha512-1HjnkKhHSDunRgIHRK4gXORl/T0WxhVkiQ5gjwvrH4yQK9RqPqYnPPwJfh+6gYTc/U9Cg8n4MGRZV1CzsP0UIA==",
+                                                                            "anonymous",
+                                                                            // "https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.15.0/js/selectize.min.js",
+                                                                            // "sha512-yPolz8xdNko3x+XY5yvuS5Inib7HXh7xD269BZOgyfv2GrNPisWLelUblxN5CdOoBAO0Siwfg4+QsAOVfUryCg==",
+                                                                            // "anonymous",
+                                                                            () =>{
+                                                                                this.include(
+                                                                                    // Papa Parse for CSVs
+                                                                                    "https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js",
+                                                                                    "sha512-SGWgwwRA8xZgEoKiex3UubkSkV1zSE1BS6O4pXcaxcNtUlQsOmOmhVnDwIvqGRfEmuz83tIGL13cXMZn6upPyg==",
+                                                                                    "anonymous",
+                                                                                    ()=>{
+                                                                                        $jq351 = jQuery.noConflict(true);
+                                                                                        // console.log("After no conflict", $.fn.jquery);
+                                                                                        // console.log("bootstrap jquery", $jq351.fn.jquery);
+                                                                                        this.myInit(mode, json_data);
+                                                                                    }
+                                                                                )
+                                                                            }
+                                                                        )
+                                                                    }
+                                                                )
+                                                            }
+                                                        );
+                                                    }
+                                                )
+                                            }
+                                        );
+                                    }
+                                )
+                            }
+                        )
+                    }
+                )
+            }
+        )
+    },
+
+    //https://stackoverflow.com/questions/8139794/load-jquery-in-another-js-file
+    include: function(src, integrity, crossorigin, onload) {
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.setAttribute("integrity", integrity);
+        script.setAttribute("crossorigin", crossorigin);
+        script.src = src;
+        script.type = 'text/javascript';
+        script.onload = script.onreadystatechange = function() {
+            if (script.readyState) {
+                if (script.readyState === 'complete' || script.readyState === 'loaded') {
+                    script.onreadystatechange = null;                                                  
+                    onload();
+                }
+            } 
+            else {
+                onload();          
+            }
+        };
+        head.appendChild(script);
+    },
+
+    myInit: function (mode, json_data) {
+       //this method is called when the form is being constructed
         // parameters
         // mode = if it equals 'view' than it should not be editable
         //        if it equals 'edit' then it will be used for entry
@@ -436,8 +535,15 @@ my_widget_script =
         $(".copyMouseIDs").on("click", (e)=>{
             var $divForCopy = $("#forCopy");
             var listName = $(e.currentTarget).data("list");
+            var numSamples = $(e.currentTarget).data("samples");
+            if(!numSamples){
+                numSamples = $("#samplesPerMouse").val();
+                if(!numSamples){
+                    numSamples = 2;
+                }
+            }
 
-            this.copyMouseIDs(listName, $divForCopy);
+            this.copyMouseIDs(listName, $divForCopy, numSamples);
         });
 
         $("#idEntry2").each((i,e)=> {
@@ -451,6 +557,18 @@ my_widget_script =
         
         $(".calcTotal").on("change", (e)=>{
             this.calcNumAssignedWells();
+        });
+
+        $("#9ptCurve").on("click", ()=>{
+            $("#numSTD").val(9);
+            $("#largestSTD").val(500);
+            this.checkAndUpdateWells();
+        });
+
+        $("#7ptCurve").on("click", ()=>{
+            $("#numSTD").val(7);
+            $("#largestSTD").val(250);
+            this.checkAndUpdateWells();
         });
         
         $("#updateEntries").on("click", ()=>{
@@ -1631,7 +1749,7 @@ my_widget_script =
         this.resize();
     },
 
-    copyMouseIDs: function(listName, $divForCopy){
+    copyMouseIDs: function(listName, $divForCopy, numCopies){
         //create a temporary text area
         var $temp = $("<text" + "area style='opacity:0;'></text" + "area>");
         var addLine = "";
@@ -1640,13 +1758,19 @@ my_widget_script =
         var splitList = fullList.split(/[\r\n]+/);
         var addText;
 
+        var newList = [];
+
         for (var i = 0; i < splitList.length /*&& i < 19*/; i++ ){
             mouseID = splitList[i];
-            $temp.text($temp.text() + addLine);
-            addLine = "\n";
-            totalText = mouseID + addLine + mouseID; // + addLine + mouseID + addLine + mouseID;
-            $temp.text($temp.text() + totalText);
+            for(var j=0; j < numCopies; j++){
+                newList.push(mouseID);
+            }
+            // $temp.text($temp.text() + addLine);
+            // addLine = "\n";
+            // totalText = mouseID + addLine + mouseID; // + addLine + mouseID + addLine + mouseID;
+            // $temp.text($temp.text() + totalText);
         }
+        $temp.text(newList.join("\n"));
 
         $temp.appendTo($divForCopy).select(); //add temp to tableDiv and select
         document.execCommand("copy"); //copy the "selected" text
