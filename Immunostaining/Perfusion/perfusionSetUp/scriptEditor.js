25c0de2355a3c22aaaed1f1396fde5ce3f5b2611
Amanda Gibson <gibsonag@umich.edu>
2024-06-13 17:55:31 -0400

    AGG/Update perfusion instructions

--- Immunostaining/Perfusion/perfusionSetUp/scriptEditor.js
+++ Immunostaining/Perfusion/perfusionSetUp/scriptEditor.js
@@ -2,6 +2,86 @@ my_widget_script =
 {
     
     init: function (mode, json_data) {
+        // jQuery for bootstrap
+        this.include(
+            "https://code.jquery.com/jquery-3.5.1.min.js",
+            "sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=",
+            "anonymous",
+            ()=>{
+                $(document).ready(
+                    ()=>{
+                        // console.log("After load", $.fn.jquery);
+                        
+                        // Load bootstrap
+                        this.include(
+                            "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js",
+                            "sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl",
+                            "anonymous",
+                            ()=>{
+                                $(document).ready(
+                                    ()=>{
+                                        // Load Luxon
+                                        this.include(
+                                            "https://cdn.jsdelivr.net/npm/luxon@1.26.0/build/global/luxon.min.js",
+                                            "sha256-4sbTzmCCW9LGrIh5OsN8V5Pfdad1F1MwhLAOyXKnsE0=",
+                                            "anonymous",
+                                            ()=>{
+                                                $(document).ready(
+                                                    ()=>{
+                                                        // Load bootbox - need the bootstrap JS to be here first, with appropriate jQuery
+                                                        this.include(
+                                                            "https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js",
+                                                            "sha512-RdSPYh1WA6BF0RhpisYJVYkOyTzK4HwofJ3Q7ivt/jkpW6Vc8AurL1R+4AUcvn9IwEKAPm/fk7qFZW3OuiUDeg==",
+                                                            "anonymous",
+                                                            // referrerpolicy="no-referrer"
+                                                            ()=>{
+                                                                $(document).ready(
+                                                                    ()=>{
+                                                                        $jq351 = jQuery.noConflict(true);
+                                                                        // console.log("After no conflict", $.fn.jquery);
+                                                                        // console.log("bootstrap jquery", $jq351.fn.jquery);
+                                                                        this.myInit(mode, json_data);
+                                                                    }
+                                                                )
+                                                            }
+                                                        );
+                                                    }
+                                                )
+                                            }
+                                        );
+                                    }
+                                )
+                            }
+                        )
+                    }
+                )
+            }
+        )
+    },
+
+    //https://stackoverflow.com/questions/8139794/load-jquery-in-another-js-file
+    include: function(src, integrity, crossorigin, onload) {
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.setAttribute("integrity", integrity);
+        script.setAttribute("crossorigin", crossorigin);
+        script.src = src;
+        script.type = 'text/javascript';
+        script.onload = script.onreadystatechange = function() {
+            if (script.readyState) {
+                if (script.readyState === 'complete' || script.readyState === 'loaded') {
+                    script.onreadystatechange = null;                                                  
+                    onload();
+                }
+            } 
+            else {
+                onload();          
+            }
+        };
+        head.appendChild(script);
+    },
+
+    myInit: function (mode, json_data) {
         //this method is called when the form is being constructed
         // parameters
         // mode = if it equals 'view' than it should not be editable
@@ -14,7 +94,7 @@ my_widget_script =
         //By default it calls the parent_class's init.
 
         //uncomment to inspect and view code while developing
-        //debugger;
+        // debugger;
 
         //Get the parsed JSON data
         var parsedJson = this.parseInitJson(json_data);
@@ -170,7 +250,7 @@ my_widget_script =
         });
 
         if (fail) { //if fail is true (meaning a required element didn't have a value)
-            return alert(fail_log); //return the fail log as an alert
+            return bootbox.alert (fail_log); //return the fail log as an alert
         } else {
             var noErrors = [];
             return noErrors;
@@ -392,6 +472,12 @@ my_widget_script =
             my_widget_script.toggleCard($(this));
         });
 
+        $("#fixSelect").each((i,e)=>{
+            this.showOther($(e));
+        }).on("click", (e)=>{
+            this.showOther($(e.currentTarget));
+        });
+
         my_widget_script.resize();
     },
 
@@ -467,7 +553,7 @@ my_widget_script =
             startTimeSplit = my_widget_script.getHoursMin(startTime);
             addTimeSplit = my_widget_script.getHoursMin(addTime);
             // set start time
-            time.setHours(startTimeSplit.hours, startTimeSplit.mins, 00, 000);
+            time.setHours(startTimeSplit.hours, startTimeSplit.mins, 0, 0);
             time.setHours(time.getHours() + addTimeSplit.hours);
             time.setMinutes(time.getMinutes() + addTimeSplit.mins);
             console.log(time);
@@ -488,5 +574,18 @@ my_widget_script =
             } 
         });
         my_widget_script.resize();
-    }
+    },
+
+    showOther: function ($el) {
+        if($el.val() === "other") {
+            // console.log("showing other");
+            var $other = $el.next(".ifOther").show();
+            // Adjust height
+            var thisScrollHeight = $other.prop("scrollHeight");
+            $other.css("height", thisScrollHeight).css("overflow-y", "hidden");
+        } else {
+            $el.next(".ifOther").hide();
+        }
+        this.resize();
+    },
 };
